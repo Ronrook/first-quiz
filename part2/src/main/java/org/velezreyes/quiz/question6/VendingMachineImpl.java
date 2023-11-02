c0e4cb93466bc59c20dcdef5cf285a4ae8200823
Ronald Rosero <ronaldes93@gmail.com>
2023-11-02 12:05:05 -0500

    part2 solution class VendingMachineImpl that implements VendingMachine interface

--- part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
+++ part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
@@ -1,9 +1,49 @@
 package org.velezreyes.quiz.question6;
 
-public class VendingMachineImpl {
+import java.util.*;
 
-  public static VendingMachine getInstance() {
-    // Fix me!
-    return null;
+public class VendingMachineImpl implements VendingMachine{
+
+  private static VendingMachine instance;
+  private double quarterDollarCount;
+  private Map<String, DrinkImple> drinks;
+
+
+  // Constructor privado
+  public VendingMachineImpl() {
+
+    this.drinks = new HashMap<>();
+    drinks.put("KarenTea", new DrinkImple("KarenTea", 0.80, false));
+    drinks.put("ScottCola", new DrinkImple("ScottCola", 0.75, true));
+
   }
+  // Patrón Singleton
+  public static VendingMachine getInstance() {
+    if ( instance == null ) {
+      instance = new VendingMachineImpl();
+    }
+    return instance;
+  }
+
+  @Override
+  public void insertQuarter(){
+    this.quarterDollarCount += 0.25;
+  }
+
+  @Override
+  public Drink pressButton(String name) throws NotEnoughMoneyException, UnknownDrinkException{
+
+    if(!drinks.containsKey(name)) {
+      throw new UnknownDrinkException();
+    }
+
+    DrinkImple drink = this.drinks.get(name);
+
+    if(quarterDollarCount < 0.1 ||quarterDollarCount < drink.getCost())
+        throw new NotEnoughMoneyException();
+
+    this.quarterDollarCount = 0;
+    return drink;
+  }
+
 }
